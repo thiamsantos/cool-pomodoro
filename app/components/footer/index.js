c978e33057c0d560cc190d9a13a52780146d7111
Thiago Santos <thia.mdossantos@gmail.com>
2017-01-18 17:06:29 -0200

    add pulse heart at footer

--- app/components/footer/index.js
+++ app/components/footer/index.js
@@ -3,5 +3,5 @@ import styles from './styles.css'
 
 export default () =>
   bel`<footer class=${styles.footer}>
-    Made with <span class="heart"></span>by <a href="https://github.com/thiamsantos" class="link" target="_blank" rel="noopener">@thiamsantos</a>
+    Made with <span class=${styles.heart}></span> by <a href="https://github.com/thiamsantos" class="link" target="_blank" rel="noopener">@thiamsantos</a>
   </footer>`
