22e637b654fa2d738f62b3c51e83792a267800d9
Thiago Santos <thia.mdossantos@gmail.com>
2017-01-19 12:35:10 -0200

    add appearance to adjusts checkboxes

--- app/components/adjusts/control.js
+++ app/components/adjusts/control.js
@@ -1,17 +1,22 @@
 import bel from 'bel'
 import {toggleAdjust} from '../../services/actions'
 import {capitalize} from '../../services/utils'
+import styles from './styles.css'
 
 const handleChange = (store, name) => () => {
   store.dispatch(toggleAdjust(name.toLowerCase()))
 }
 
 export default (store, name) =>
-  bel`<div>
+  bel`<div class=${styles.control}>
     <input
       type="checkbox"
       id="${name}"
+      class=${styles.input}
       onchange=${handleChange(store, name)}
       checked=${store.getState().adjusts[name]} >
-    <label for="${name}">${capitalize(name)}</label>
+    <label for="${name}" class=${styles.label}>
+      <span class=${styles.box}></span>
+      ${capitalize(name)}
+    </label>
   </div>`
