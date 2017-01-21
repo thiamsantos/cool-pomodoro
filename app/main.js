bcd9f2ead9d1e6f4be5aa7b197b3e90a3f24395c
Thiago Santos <thia.mdossantos@gmail.com>
2017-01-21 21:01:13 -0200

    add vibration support closes #13

--- app/main.js
+++ app/main.js
@@ -7,20 +7,32 @@ import Root from './components/root'
 document.body.appendChild(Root(store))
 
 const render = () => {
-  // console.log(store.getState())
+  console.log(store.getState())
   document.title = getTitle(store)
   morphdom(document.getElementById('app'), Root(store))
 }
 
+const notify = () => {
+  const state = store.getState()
+
+  if (state.timer.value === 0) {
+    if (state.adjusts.vibration && 'vibrate' in window.navigator) {
+      window.navigator.vibrate(1000)
+    }
+  }
+}
+
 store.subscribe(render)
+store.subscribe(notify)
 
 setInterval(() => {
-  if (store.getState().timer.playState === 'paused') {
+  const state = store.getState()
+
+  if (state.timer.playState === 'paused' || state.timer.value === 0) {
     return false
   }
-  store.dispatch({
-    type: 'DECREMENT_TIMER'
-  })
+
+  store.dispatch({type: 'DECREMENT_TIMER'})
 }, 1000)
 
 document.addEventListener('keyup', keyboardShortcuts(store))
